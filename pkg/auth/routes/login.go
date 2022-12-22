cb191523fae1f73cec138898d30ed6fdeaacaadb
Agung Nugroho <34261318+agungnugroho24@users.noreply.github.com>
2022-12-22 16:37:00 +0700

    Update login.go

--- pkg/auth/routes/login.go
+++ pkg/auth/routes/login.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/hellokvn/go-grpc-api-gateway/pkg/auth/pb"
+	"github.com/agungnugroho24/go-grpc-api-gateway/pkg/auth/pb"
 )
 
 type LoginRequestBody struct {
