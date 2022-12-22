9bdf1d81f3fb8ea2036d4e4d2c375728fcf8911d
Agung Nugroho <34261318+agungnugroho24@users.noreply.github.com>
2022-12-22 16:40:16 +0700

    Update create_order.go

--- pkg/order/routes/create_order.go
+++ pkg/order/routes/create_order.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/hellokvn/go-grpc-api-gateway/pkg/order/pb"
+	"github.com/agungnugroho24/go-grpc-api-gateway/pkg/order/pb"
 )
 
 type CreateOrderRequestBody struct {
