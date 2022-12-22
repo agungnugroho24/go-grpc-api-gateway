e1b564c43b5e03c111bb61f499968f44ec94a389
Agung Nugroho <34261318+agungnugroho24@users.noreply.github.com>
2022-12-22 16:36:41 +0700

    Update client.go

--- pkg/auth/client.go
+++ pkg/auth/client.go
@@ -3,8 +3,8 @@ package auth
 import (
 	"fmt"
 
-	"github.com/hellokvn/go-grpc-api-gateway/pkg/auth/pb"
-	"github.com/hellokvn/go-grpc-api-gateway/pkg/config"
+	"github.com/agungnugroho24/go-grpc-api-gateway/pkg/auth/pb"
+	"github.com/agungnugroho24/go-grpc-api-gateway/pkg/config"
 	"google.golang.org/grpc"
 )
 
